a73edea06ebaea38da7d21f63154019f54e7c0dc
Michael Peng <michael@mcmoo.org>
2020-05-02 11:22:33 -0400

    Add DSL intermediate model

---
+++ src/dimscommander/dsl/model.nim
@@ -0,0 +1,28 @@
+import options
+
+# V-style
+template `?`(T: typedesc): typedesc = Option[T]
+
+type
+  InputType = enum
+    Int, Float, String, Varargs
+    
+  InputLimits = object
+    case inputType: InputType
+    of Int, Float:
+      min, max: ?float
+    of String:
+      minLength, maxLength: ?uint
+    of Varargs:
+      argType: InputType
+    
+  Argument = object
+    label: ?string
+    limits: InputLimits
+    
+  CommandDef = ref object
+    name: string
+    help: tuple[name: ?string, description: ?string]
+    args: ?seq[Argument]
+    handler: NimNode
+  
